9466f74eae7528ffe8c2f456fac0e4ec9070e71c
Kengo Wada <louiskengo3@gmail.com>
2020-01-08 08:32:50 +0300

    Added solution 1

---
+++ quiz/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("problems.csv")
+	if err != nil {
+		fmt.Println("Error reading file")
+	}
+	defer file.Close()
+
+	csvr := csv.NewReader(file)
+
+	correctCounter := 0
+	questionCounter := 0
+
+	for {
+		row, err := csvr.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			panic(err)
+		}
+
+		question := row[0]
+		answer := row[1]
+
+		fmt.Println(question)
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter text: ")
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSuffix(text, "\n")
+
+		if text == answer {
+			correctCounter++
+		}
+		questionCounter++
+	}
+
+	fmt.Printf("You got %v correct out of %v\n", correctCounter, questionCounter)
+}
